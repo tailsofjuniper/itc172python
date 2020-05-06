f7d21cb6e81f8b712bb7f90b014802c85d62ad3e
tailsofjuniper <tailsofjuniper@gmail.com>
2020-05-05 19:51:25 -0700

    commit

--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('Club/', include('Club.urls'))
 ]
