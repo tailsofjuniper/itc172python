2a60b9dff73ae062dad29e05b63d795e582a7f64
tailsofjuniper <tailsofjuniper@gmail.com>
2020-05-31 18:36:03 -0700

    commit

--- club/views.py
+++ club/views.py
@@ -1,5 +1,9 @@
+from .models import Meeting, MeetingMinutes, Resource, Event
 from django.shortcuts import render
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+def getresources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'club/resource.html' ,{'resource_list' : resource_list})
