f8113aeed995acc4a9cc93f6fa1e4c7051d0c740
tailsofjuniper <tailsofjuniper@gmail.com>
2020-06-03 00:41:04 -0700

    commit

--- club/urls.py
+++ club/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('resource/', views.getresource, name='resource')
+    path('resource/', views.getresource, name='resource'),
+    path('meeting/', views.getmeeting, name='meeting')
+    # path('minutes/', views.getminutes, name='minutes')
+    # path('event/', views.getevent, name='event')
 ]
--- club/views.py
+++ club/views.py
@@ -7,3 +7,6 @@ def index (request):
 def getresource (request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resource.html' ,{'resource_list' : resource_list})
+def getmeeting (request):
+    meeting_list=Meeting.objects.all()
+    return render(request, 'club/meeting.html' ,{'meeting_list' : meeting_list})
