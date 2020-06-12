2d4cd752da02fd415f06f5f3cff7e1afc365f87e
tailsofjuniper <tailsofjuniper@gmail.com>
2020-06-11 20:10:56 -0700

    commit

--- club/views.py
+++ club/views.py
@@ -7,14 +7,16 @@ def index (request):
 def getresource (request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resource.html' ,{'resource_list' : resource_list})
-def getmeeting (request, id):
+def getmeeting (request):
     meeting_list=Meeting.objects.all()
     return render(request, 'club/meeting.html' ,{'meeting_list' : meeting_list})
 def meetingdetails (request, id):
     meeting_detail=get_object_or_404(Meeting, pk=id)
+    meeting_location=meeting_detail.meetinglocation
     # discount=prod.memberdiscount
     # reviews=Review.objects.filter(product=id).count()
     context={
         'meeting_detail' : meeting_detail,
+        'meeting_location' : meeting_location,
     }
     return render(request, 'club/meetingdetails.html', context=context)
