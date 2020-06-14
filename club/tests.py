d8d32fdd78befd7910f93913fcdbaf1a38585fd2
tailsofjuniper <tailsofjuniper@gmail.com>
2020-06-13 18:28:16 -0700

    commit

--- club/tests.py
+++ club/tests.py
@@ -1,3 +1,12 @@
+import unittest
+from meeting import Meeting
+from meetingdetail import MeetingDetail
 from django.test import TestCase
-
+from .models import Meeting, MeetingMinutes, Resource, Event
 # Create your tests here.
+class MeetingTypeTest(TestCase):
+    def setup(self):
+        type = Meeting(typename='Anniversary')
+        
+    def test_string(self):
+        type=Meeting
